f7922dc5ef152e23032a624c7ed5143c42224372
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2020-11-18 21:01:06 +0300

    1-logout-feature

--- client/src/Common/components/Header/Header.js
+++ client/src/Common/components/Header/Header.js
@@ -4,6 +4,7 @@ import {useHistory} from 'react-router-dom'
 import {AuthContext} from 'App/context/AuthContext'
 import Button from "Common/components/Button/Button";
 import {User} from "Common/shared/User";
+import logout from 'Common/assets/images/logout.svg'
 
 export function Header() {
   const history = useHistory()
@@ -15,20 +16,38 @@ export function Header() {
     history.push('/login')
   }
 
+
     return (
       <div className='Header'>
         <a className='logo' href="/"></a>
 
-        <form className='search' >
+        <form className='search'>
           <input type="text" className="search-input" placeholder="Search"/>
         </form>
 
         <div className='user'>
           <User name={auth.userName} styleName='User-header'/>
+          <div className='Navbar'>
+            <div className='navbar-user__menu'>
+              <div className='menu --vertical'>
+                <ul className='menu__list'>
+                  <li className='menu__item'>
+                    <button className='menu__link'>
+                      <span className='menu__icon'>
+                        <img src={logout} alt=""/>
+                      </span>
+                      <span className='menu__text'>Выйти</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+        </div>
         </div>
 
 
-        <Button name=' Log out' logoutHandler={logoutHandler}/>
+        {/*<Button name='Log out' logoutHandler={logoutHandler}/>*/}
       </div>
     )
 }
+
