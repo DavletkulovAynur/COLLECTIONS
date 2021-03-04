740b71181a99953d440a913f2213e58a2cb58029
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2021-03-04 13:19:46 +0300

    feature-drop-area

--- client/src/App/pages/AddCollection/AddCollectionTemplate.js
+++ client/src/App/pages/AddCollection/AddCollectionTemplate.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import {Select} from "../../../Common/components/Select/Select";
+import React from 'react'
 
 const AddCollectionTemplate = ({title, img, publisher, description, testSelect, handleSubmit}) => {
 
@@ -8,33 +7,46 @@ const AddCollectionTemplate = ({title, img, publisher, description, testSelect, 
       <input className='com-input-styles' type='text' placeholder={placeholder} {...input.bind}/>
     )
   }
+  function dropArea() {
+    return (
+      <div id="drop-area-js" className="drop-area">
+        <form>
+          <div className="cloud">
+            <div className="icon"></div>
+            <i className="text">Кликните или перетащите, чтобы загрузить фотографии</i>
+          </div>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div className='Add-collection'>
       <h1>ADD GAME</h1>
       <div className='form'>
         <form onSubmit={() => handleSubmit()}>
-          <Select options={{
-            data: [
-              {id: '1', value: 'React'},
-              {id: '2', value: 'Angular'},
-              {id: '3', value: 'Vue'},
-              {id: '4', value: 'React Native'},
-              {id: '5', value: 'Next'},
-              {id: '5', value: 'Nest'}
-            ],
-          }}/>
-          <select {...testSelect.bind}>
-            <option>book</option>
-            <option>movie</option>
-            <option>game</option>
-            <option>photo</option>
-          </select>
+          {/*<Select options={{*/}
+          {/*  data: [*/}
+          {/*    {id: '1', value: 'React'},*/}
+          {/*    {id: '2', value: 'Angular'},*/}
+          {/*    {id: '3', value: 'Vue'},*/}
+          {/*    {id: '4', value: 'React Native'},*/}
+          {/*    {id: '5', value: 'Next'},*/}
+          {/*    {id: '5', value: 'Nest'}*/}
+          {/*  ],*/}
+          {/*}}/>*/}
+          {/*<select {...testSelect.bind}>*/}
+          {/*  <option>book</option>*/}
+          {/*  <option>movie</option>*/}
+          {/*  <option>game</option>*/}
+          {/*  <option>photo</option>*/}
+          {/*</select>*/}
+          {dropArea()}
           {inputForm('title', title)}
           {inputForm('img', img)}
           {inputForm('publisher', publisher)}
           {inputForm('description', description)}
-          <input type="submit" className="btn btn-primary" value="SEND"/>
+          <input type="submit" className="com-btn-styles btn-primary" value="SEND"/>
         </form>
       </div>
     </div>
