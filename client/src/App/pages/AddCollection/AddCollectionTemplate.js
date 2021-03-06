05746558d1359317a9e8e333364ba9800e673b80
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2021-03-06 21:43:34 +0300

    feature-text-draft

--- client/src/App/pages/AddCollection/AddCollectionTemplate.js
+++ client/src/App/pages/AddCollection/AddCollectionTemplate.js
@@ -1,7 +1,16 @@
-import React, {useState} from 'react'
+import React, {Component, useState} from 'react'
 import {InputBase, makeStyles, TextField} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
+
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+
+
 
 import {DragAndDrop} from '../../../Common/components/DragAndDrop/DragAndDrop';
 
@@ -27,6 +36,7 @@ const AddCollectionTemplate = ({title, publisher,  description, saveImages, hand
           <TextField {...description.bind} id="filled-multiline-static" label="description" multiline rows={4} placeholder="Default Value" variant="filled"
                      classes={{root: classes.root}}/>
 
+          <EditorConvertToHTML/>
 
           <Button
             variant="contained"
@@ -64,3 +74,33 @@ const useStyles = makeStyles({
 });
 
 export default AddCollectionTemplate;
+
+class EditorConvertToHTML extends Component {
+  state = {
+    editorState: EditorState.createEmpty(),
+  }
+
+  onEditorStateChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  render() {
+    const { editorState } = this.state;
+    return (
+            <div>
+              <Editor
+                      editorState={editorState}
+                      wrapperClassName="demo-wrapper"
+                      editorClassName="demo-editor"
+                      onEditorStateChange={this.onEditorStateChange}
+              />
+              <textarea
+                      disabled
+                      value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+              />
+            </div>
+    );
+  }
+}
