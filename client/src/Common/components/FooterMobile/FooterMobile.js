202b4cf5162b6a1bd4feb817cb832bd3d201bd25
Davletkulov Aynur <davletkulovaynur@MacBook-Air-Davletkulov.local>
2021-08-02 07:17:10 +0300

    feature-main-page-style

--- client/src/Common/components/FooterMobile/FooterMobile.js
+++ client/src/Common/components/FooterMobile/FooterMobile.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './FooterMobile.scss'
 
 export function FooterMobile() {
   return (
     <div className='Footer-mobile'>
         <div className='icons-wrapper'>
-          <div className='icon'></div>
-          <div className='icon loupe'></div>
-          <div className='icon plus'></div>
-          <div className='icon user'></div>
+          <Link to='/'>
+            <div className='icon'></div>
+          </Link>
+          <Link to='/search-mobile'>
+            <div className='icon loupe'></div>
+          </Link>
+          <Link to='/add'>
+            <div className='icon plus'></div>
+          </Link>
+          <Link to='/personal-area'>
+            <div className='icon user'></div>
+          </Link>
         </div>
     </div>
   )
