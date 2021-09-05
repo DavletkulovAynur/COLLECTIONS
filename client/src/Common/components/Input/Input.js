6ecba3917babc847cd10a1ec4db0d240ddfb776a
Davletkulov Aynur <davletkulovaynur@MacBook-Air-Davletkulov.local>
2021-09-05 15:09:56 +0300

    login-new-design

--- client/src/Common/components/Input/Input.js
+++ client/src/Common/components/Input/Input.js
@@ -1,11 +1,5 @@
 import React, {useState} from 'react';
-import {makeStyles, TextField} from '@material-ui/core'
 import './Input.scss'
-
-import InputAdornment from '@material-ui/core/InputAdornment'
-import IconButton from '@material-ui/core/IconButton'
-import Visibility from '@material-ui/icons/Visibility'
-import VisibilityOff from '@material-ui/icons/VisibilityOff'
 
 const Input = ({error = '',
                binding,
@@ -15,87 +9,31 @@ const Input = ({error = '',
                rows = 1,
                 password}) => {
 
-    const classes = useStyles();
-
     const [showPassword, setShowPassword] =useState(false)
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword)
   };
 
+    const inputClear = () => {
+
+    }
+
+    console.log(error)
+
+    const errorStyle = error ? 'Input-root__error' : ''
 
     return (
-        <>
-            <TextField error={!!error}
-                       {...binding.bind}
-                       required={requiredTest}
-                       multiline={multiline}
-                       rows={rows}
-                       id="filled-basic"
-                       label={label}
-                       variant="filled"
-
-
-                       InputLabelProps={{classes: {root: classes.test, focused: classes.test}}}
-                       classes={{root: classes.root}}
-                       InputProps={{
-                         classes: {underline: classes.underline, input: classes.input},
-                         endAdornment: (
-                           password
-                               ? <InputAdornment position="end">
-                                    <IconButton
-                                      color='inherit'
-                                      aria-label="Toggle password visibility"
-                                      onClick={handleClickShowPassword}
-                                    >
-                                    {  showPassword ? <VisibilityOff /> : <Visibility />}
-                                  </IconButton>
-                                </InputAdornment>
-                              : null
-                         ),
-                       }}
-            />
-        </>
+        <div className={`Input-root ${errorStyle}`}>
+          <div className={`input-group`}>
+              <input {...binding.bind} className='input-group_input' type="text" required/>
+              <span className="input-group_highlight"></span>
+              <span className="input-group_bar"></span>
+              <label>{label}</label>
+          </div>
+        </div>
     );
 }
 
 export default Input
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      background: 'rgba(256, 256, 256, 0.2);',
-      borderRadius: 4,
-      width: '100%',
-      color: '#fff'
-    },
-    label: {
-      textTransform: 'capitalize',
-      color: 'white',
-    },
-    multilineColor:{
-      color:'red',
-      '&::focused': {
-        color: 'red',
-      }
-    },
-    test: {
-        color: '#888888',
-    },
-    focused: {
-        color: '#888888',
-    },
-    input: {
-      color: 'white',
-      "&:-webkit-autofill": {
-        WebkitBoxShadow: "0 0 0 1000px #54585c inset",
-        color: '#fff',
-        WebkitTextFillColor: '#fff'
-      }
-    },
-    underline: {
-      // '&::after': {
-      //   border: '1px solid #fff'
-      // }
-    }
-
-}))
